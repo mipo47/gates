4033bce19f93058691c4adce01c9d1f0a068503c
mipo47 <miroslav@fatmap.com>
2017-08-03 03:03:58 +0300

    weight checkpoint

---
+++ checkpoint.py
@@ -0,0 +1,25 @@
+import numpy as np
+from gates.gate import *
+
+
+class Checkpoint:
+    def __init__(self, net):
+        self.net = net
+        self.weights = None
+
+    def backup(self):
+        self.weights = []
+        gate = self.net
+        while gate is not None:
+            if isinstance(gate, GateWeights):
+                self.weights.append(np.copy(gate.w))
+            gate = gate.prev
+
+    def restore(self):
+        gate = self.net
+        i = 0
+        while gate is not None:
+            if isinstance(gate, GateWeights):
+                gate.w = self.weights[i]
+                i += 1
+            gate = gate.prev
