3a706313356f8f9f908c7d875b26ba15cb2f8ac7
mipo47 <miroslav@fatmap.com>
2017-08-17 06:00:18 +0300

    Elegant integration with .net

--- dot_net.py
+++ dot_net.py
@@ -1,8 +1,16 @@
+import sys
+import clr
+# same path is set as working directory
+sys.path.append(r"G:\Projects\ML\gate-net\GatesNet\GatesNet\bin\Debug")
+clr.AddReference("GatesNet")
+
 from System import Int32, Array, Single
 from System.Runtime.InteropServices import GCHandle, GCHandleType
+from GatesNet.Gates import GArray
+
 import ctypes
 import numpy as np
-
+from gates.gate import *
 
 def net2numpy(net_array):
     src_hndl = GCHandle.Alloc(net_array, GCHandleType.Pinned)
@@ -26,3 +34,23 @@ def numpy2net(np_array):
         for i2 in range(np_array.shape[1]):
             x[i1, i2] = np_array[i1, i2]
     return x
+
+
+class FromDotNet(Gate):
+    # prev is GateNet
+    def __init__(self, prev):
+        super().__init__(size=prev.Size)
+        self.prev = prev
+
+    def forward(self, value):
+        net_input = numpy2net(value)
+        gpu_input = GArray(net_input)
+        gpu_output = self.prev.Forward(gpu_input)
+        net_output = gpu_output.Data
+        self.value = net2numpy(net_output)
+        return self.value
+
+    def backward(self, gValue, optimizer):
+        net_input = numpy2net(gValue)
+        gpu_input = GArray(net_input)
+        self.prev.Backward(gpu_input, None)
