b09a8edec12ad597f56ba3ad46f957e3b2ba379b
mipo47 <miroslav@fatmap.com>
2017-09-21 14:27:21 +0300

    Pendulum - try custom regularization, solve -134.1

--- regularization.py
+++ regularization.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Regularizer:
     def __init__(self, optimizer, regularization):
         self.optimizer = optimizer
@@ -7,6 +10,16 @@ class Regularizer:
         gW += w * self.regularization
         self.optimizer.update(w, gW)
 
+
+class RegularizerDir:
+    def __init__(self, optimizer, regularization):
+        self.optimizer = optimizer
+        self.regularization = regularization
+
+    def update(self, w, gW):
+        mult = 1.0 + np.abs(w) * self.regularization
+        gW *= mult ** (np.sign(gW) * np.sign(w))
+        self.optimizer.update(w, gW)
 
 # class RegularizerL2:
 #     def __init__(self, optimizer, regularization):
